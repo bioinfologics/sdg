aee76df6c55ad304d4327f0da940d5269d8844b2
Katie Barr <katarzinia@gmail.com>
2017-11-14 14:09:26 +0000

    load names of bubbles from file

--- src/haplotype_scorer.cpp
+++ src/haplotype_scorer.cpp
@@ -13,31 +13,43 @@ void HaplotypeScorer::find_possible_haplotypes(int max_degree, std::string bubbl
     std::string line;
     std::string fields[max_degree];
     std::cout << "Loading bubbles " << bubble_contig_filename << std::endl;
-
+    int counter = 0;
+    std::string res;
+    // no obvious way to access graph nodes directly, so store bubble names;
+    std::vector<std::vector<std::string> > bubbles;
     while (std::getline(infile, line)) {
-        std::string previous = "";
-        std::string current;
-        std::string current2;
-        std::cout << line << std::endl;
-        for (int i=0; i < max_degree; i++){
-            std::istringstream(line) >> current;
-            std::istringstream(line) >> current2;
-            std::cout << "i: " << i << "Current: " << current  << " Current2: " << current2 << " previous " << previous << std::endl;
-            if (current!= previous) {
-                fields[i] = current;
-                previous = fields[i];
-            } else {
-                //  get same contig name twice when its run past last column
-                break;
+        std::istringstream(line) >> res;
+        if (res == "next" or counter == max_degree){
+            std::cout << "next" << std::endl;
+            std::vector<std::string> next_bubble;
+            for (int j=0; j < counter; j++){
+                std::cout << "addng: " << fields[j] << std::endl;
+                next_bubble.push_back(fields[j]);
             }
+
+            counter = 0;
+            bubbles.push_back(next_bubble);
+            for (auto b: bubbles.back()){
+                std::cout << "b: " << b << std::endl;
+            }
+
+            continue;
+        } else {
+            std::cout << "res: " << res << std::endl;
+            fields[counter] = res;
+            counter += 1;
+
         }
-        std::cout << "Bubble \n";
-        for (int i=0; i < max_degree; i++){
-            std::cout << fields[i] << " ";
-        }
-        std::cout << "\n";
 
     }
+    std::cout << "Bubble \n";
+    for (auto b:bubbles){
+        for (auto c:b){
+            std::cout << c << " ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
 };
 void HaplotypeScorer::count_barcode_votes(){};
 void HaplotypeScorer::score_haplotypes(){};
