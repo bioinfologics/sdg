7e9a8df45cec203af8c1d63d6f831a8978a60f93
Luis <yanes.luis@gmail.com>
2017-11-15 23:26:17 +0000

    comp reserve

--- src/comp_kc.cc
+++ src/comp_kc.cc
@@ -12,6 +12,8 @@
 #include <limits>
 #include <iterator>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "sglib/KMerIDX.h"
 
 
@@ -23,33 +25,29 @@ off_t getFilesize(const std::string& filename) {
     return st.st_size;
 }
 
-std::vector<KmerIDX> readKC(std::string filename) {
-    std::ifstream ref_kc(filename, std::ios_base::binary);
-    ref_kc.unsetf(std::ios_base::skipws);
+std::pair<KmerIDX*,uint64_t> readKC(std::string filename) {
 
-    ref_kc.seekg(0, std::ios_base::end);
-    auto fileSize = getFilesize(filename);
-    ref_kc.seekg(sizeof(uint64_t), std::ios_base::beg);
+    auto in_fds = ::open(filename.c_str(), O_RDONLY);
+    uint64_t size;
+    ::read(in_fds, &size, sizeof(size));
     // reserve capacity
-    std::vector<KmerIDX> vec;
-    vec.reserve(fileSize/sizeof(KmerIDX));
-
+    KmerIDX* vec = (KmerIDX*) malloc(size*sizeof(KmerIDX));
     // read the data:
-    vec.insert(vec.begin(), std::istream_iterator<KmerIDX>(ref_kc), std::istream_iterator<KmerIDX>());
-    return vec;
+    uint64_t numElements = ::read(in_fds, (char *) vec, size*sizeof(KmerIDX)) / sizeof(KmerIDX);
+    return std::make_pair(vec,numElements);
 }
 
 int main(int argc, char **argv){
 
-    std::vector<KmerIDX> ref_idx(readKC(std::string(argv[1])));
-    std::vector<KmerIDX> asm_idx(readKC(std::string(argv[2])));
+    std::pair<KmerIDX*,uint64_t> ref_idx(readKC(std::string(argv[1])));
+    std::pair<KmerIDX*,uint64_t> asm_idx(readKC(std::string(argv[2])));
 
     size_t idxRef(0), idxAsm(0);
-    while (idxRef<ref_idx.size() and idxAsm < asm_idx.size()) {
-        if (asm_idx[idxAsm].kmer == ref_idx[idxRef].kmer) {
-            std::cout << ref_idx[idxRef] << "\t" << asm_idx[idxAsm] << std::endl;
+    while (idxRef<ref_idx.second and idxAsm < asm_idx.second) {
+        if (asm_idx.first[idxAsm].kmer == ref_idx.first[idxRef].kmer) {
+            std::cout << ref_idx.first[idxRef] << "\t" << asm_idx.first[idxAsm] << std::endl;
             idxAsm++; idxRef++;
-        } else if (ref_idx[idxRef].kmer < asm_idx[idxAsm].kmer){
+        } else if (ref_idx.first[idxRef].kmer < asm_idx.first[idxAsm].kmer){
             idxRef++;
         }
         else {
