e8bb6f2ef22836ad4aa1c79b2ebaa93672fbccfa
Luis <yanes.luis@gmail.com>
2018-01-16 17:17:51 +0000

    Add googletest for TDD.

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 # Bash Script that builds project
-
 mkdir build
 cd build
 cmake .. ${CMAKE_OPTIONS}
-make
+make -j8
 make test
+ctest
 touch ./docs/html/.nojekyll || true
 lcov --directory . --capture --output-file coverage.info
 lcov --remove coverage.info '/usr/*' --output-file coverage.info
