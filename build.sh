55f885fcc9b96674cb71cb66aa9a9e37c7b94ec5
Luis <yanes.luis@gmail.com>
2018-01-17 14:05:18 +0000

    Add googletest for TDD.

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ mkdir build
 cd build
 cmake .. ${CMAKE_OPTIONS}
 make -j8
+
+find . -iname "*.gcda"
+
 PATH=$PATH:./bin make test
 PATH=$PATH:./bin ctest
+
+find . -iname "*.gcda"
 
 touch ./docs/html/.nojekyll || true
 lcov --directory . --capture --output-file coverage.info
