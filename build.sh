197a44b45e97320dedd17a11b50bb75eb3421f2f
Luis <yanes.luis@gmail.com>
2018-01-17 14:41:10 +0000

    Add googletest for TDD.

--- build.sh
+++ build.sh
@@ -13,10 +13,3 @@ PATH=$PATH:./bin ctest
 find . -iname "*.gcda"
 
 touch ./docs/html/.nojekyll || true
-lcov --directory . --capture --output-file coverage.info
-lcov --remove coverage.info '/usr/*' --output-file coverage.info
-lcov --remove coverage.info '*Applications*' --output-file coverage.info
-lcov --remove coverage.info '*googletest*' --output-file coverage.info
-lcov --remove coverage.info '*v1*' --output-file coverage.info
-lcov --list coverage.info
-bash <(curl -s https://codecov.io/bash)
