516f8a9fd6b11d7136cebd0b73e45ddda2dc2171
Bernardo J. Clavijo <bjclavijo@gmail.com>
2018-02-25 16:02:29 +0000

    workspace, skeleton only

---
+++ src/sglib/WorkSpace.hpp
@@ -0,0 +1,42 @@
+//
+// Created by Bernardo Clavijo (EI) on 24/02/2018.
+//
+
+#ifndef BSG_WORKSPACE_HPP
+#define BSG_WORKSPACE_HPP
+
+
+#include <sglib/datastores/LinkedReadsDatastore.hpp>
+#include <sglib/mappers/LinkedReadMapper.hpp>
+#include "SequenceGraph.hpp"
+#include "KmerCompressionIndex.hpp"
+class LogEntry{
+public:
+    LogEntry(std::time_t t, std::string v, std::string tx):timestamp(t),bsg_version(v),log_text(tx){};
+    std::time_t timestamp;
+    std::string bsg_version;
+    std::string log_text;
+};
+class WorkSpace {
+
+public:
+    WorkSpace():kci(sg){};
+    void print_log();
+
+    void add_log_entry(std::string text);
+
+    void dump_to_disk(std::string filename);
+
+    void load_from_disk(std::string filename,bool log_only=false);
+
+    std::vector<LogEntry> log;
+
+    //All status classes are public, treat them with care anyway ;)
+    SequenceGraph sg;
+    std::vector<LinkedReadsDatastore> linked_read_datastores;
+    std::vector<LinkedReadMapper> linked_read_mappers;
+    KmerCompressionIndex kci;
+};
+
+
+#endif //BSG_WORKSPACE_HPP
