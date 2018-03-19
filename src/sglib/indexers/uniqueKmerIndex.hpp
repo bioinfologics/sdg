66c1fdcf36e98f861042acc7878f1730b456863b
Luis Yanes <yanes.luis@gmail.com>
2018-03-19 12:39:18 +0000

    Make uniqueKmerIndex serializable

--- src/sglib/indexers/uniqueKmerIndex.hpp
+++ src/sglib/indexers/uniqueKmerIndex.hpp
@@ -18,10 +18,14 @@
 #include <sglib/types/KmerTypes.hpp>
 
 class uniqueKmerIndex {
-    std::unordered_map<uint64_t, graphPosition> kmer_to_graphposition;
+    using Map = std::unordered_map<uint64_t, graphStrandPos>;
+    using const_iterator = std::unordered_map<uint64_t, graphStrandPos>::const_iterator;
+    using pair = std::pair<uint64_t, graphStrandPos>;
+
+    Map kmer_to_graphposition;
     uint k;
 public:
-    uniqueKmerIndex(uint k, uint memlimit = 4) : k(k) {}
+    explicit uniqueKmerIndex(uint k, uint memlimit = 4) : k(k) {}
 
     uniqueKmerIndex(const SequenceGraph &sg, uint k, uint64_t memlimit = 4) :
             k(k)
@@ -56,6 +60,36 @@ public:
     std::unordered_map<uint64_t, graphPosition>::const_iterator end() {
         return kmer_to_graphposition.cend();
     };
+
+    void write_to_file(std::string& filename) {
+        sglib::OutputLog() << "Dumping index" << std::endl;
+        std::ofstream outf(filename, std::ios_base::binary);
+        auto mapSize(kmer_to_graphposition.size());
+        outf.write(reinterpret_cast<const char *>(&k), sizeof(k));
+        outf.write(reinterpret_cast<const char *>(&mapSize), sizeof(mapSize));
+        for (const auto &skm: kmer_to_graphposition){
+            outf.write(reinterpret_cast<const char *>(&skm.first), sizeof(skm.first));
+            outf.write(reinterpret_cast<const char *>(&skm.second), sizeof(skm.second));
+        }
+        sglib::OutputLog() << "Done!" << std::endl;
+
+    }
+
+    void read_from_file(std::string& filename) {
+        sglib::OutputLog() << "Loading index" << std::endl;
+        std::ifstream outf(filename, std::ios_base::binary);
+        pair skm{0,graphStrandPos(0,0)};
+        auto mapSize(kmer_to_graphposition.size());
+        kmer_to_graphposition.reserve(mapSize);
+        outf.read(reinterpret_cast<char *>(&k), sizeof(k));
+        outf.read(reinterpret_cast<char *>(&mapSize), sizeof(mapSize));
+        for (decltype(kmer_to_graphposition.size()) n = 0; n < mapSize; ++n){
+            outf.read(reinterpret_cast<char *>(&skm.first), sizeof(skm.first));
+            outf.read(reinterpret_cast<char *>(&skm.second), sizeof(skm.second));
+            kmer_to_graphposition.emplace(skm.first, skm.second);
+        }
+        sglib::OutputLog() << "Done!" << std::endl;
+    }
 };
 
 
