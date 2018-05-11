1e22b3d804de530e5498ffb59f92f53f867174e2
Gonzalo Garcia <gonzalogacc@gmail.com>
2018-05-11 17:58:24 +0100

    no dead nodes, threhols kci, parallel kci profile

--- src/sglib/KmerCompressionIndex.cc
+++ src/sglib/KmerCompressionIndex.cc
@@ -5,6 +5,8 @@
 
 #include "KmerCompressionIndex.hpp"
 #include <atomic>
+#include <backward/strstream>
+#include <sstream>
 
 
 void KmerCompressionIndex::index_graph(){
@@ -330,19 +332,27 @@ void KmerCompressionIndex::compute_kci_profiles(std::string filename, std::vecto
     // vector to store vector of zero counts
     std::ofstream of(filename+"_kci.csv");
 
+
 //    for (sgNodeID_t n: node_whitelist){
-    for (auto n=1; n<sg.nodes.size(); ++n)
-        of << "seq" << n <<" | ";
+#pragma omp parallel for
+    for (auto n=1; n<sg.nodes.size(); ++n){
+        if (sg.nodes[n].status == sgNodeDeleted) continue;
+        std::stringstream ss;
+        ss << "seq" << n <<" | ";
+        // TODO: complete this to throw a warning when accessing a deleted node
+        //std::cout << "WARINIG: " <<std::endl;
         for (auto var=0; var < read_counts.size(); ++var) {
             auto zero_count = 0;
             auto read_coverage = compute_node_coverage_profile(sg.nodes[n].sequence, var);
             for (auto c: read_coverage[0]) {
-                if (c == 0){
+                // TODO: Corregir este threhold
+                if (c < 3){
                     zero_count++;
                 }
             }
-            of << zero_count << ",";
+            ss << zero_count << ",";
         }
-        of << std::endl;
+#pragma omp critical
+        of << ss.str() << std::endl;
     }
 }
