de7001c4e63886cf58187f8b356396b058de8dfb
Bernardo J. Clavijo <bjclavijo@gmail.com>
2018-05-25 19:08:50 +0100

    LinkageDiGraph

---
+++ src/sglib/LinkageDiGraph.cc
@@ -0,0 +1,93 @@
+//
+// Created by Bernardo Clavijo (EI) on 25/05/2018.
+//
+
+#include "LinkageDiGraph.hpp"
+void LinkageDiGraph::add_link(sgNodeID_t source, sgNodeID_t dest, int32_t d) {
+    if (llabs(source)>=links.size()) links.resize(llabs(source)+1);
+    if (llabs(dest)>=links.size()) links.resize(llabs(dest)+1);
+    Link l(source,dest,d);
+    links[(source > 0 ? source : -source)].emplace_back(l);
+    std::swap(l.source,l.dest);
+    links[(dest > 0 ? dest : -dest)].emplace_back(l);
+}
+
+void LinkageDiGraph::remove_link(sgNodeID_t source, sgNodeID_t dest) {
+    if (llabs(source)>=links.size() or llabs(dest)>=links.size()) return;
+    auto & slinks = links[(source > 0 ? source : -source)];
+    slinks.erase(std::remove(slinks.begin(), slinks.end(), Link(source,dest,0)), slinks.end());
+    auto & dlinks = links[(dest > 0 ? dest : -dest)];
+    dlinks.erase(std::remove(dlinks.begin(), dlinks.end(), Link(dest,source,0)), dlinks.end());
+
+}
+
+std::vector<Link> LinkageDiGraph::get_fw_links( sgNodeID_t n){
+    std::vector<Link> r;
+    if (llabs(n)>=links.size()) return r;
+    for (auto &l:links[(n>0 ? n : -n)]) if (l.source==-n) r.emplace_back(l);
+    return r;
+}
+
+std::vector<Link> LinkageDiGraph::get_bw_links(sgNodeID_t n) {
+    return get_fw_links (-n);
+}
+
+//returns a list of all fw nodes up to radius jumps away.
+std::set<sgNodeID_t> LinkageDiGraph::fw_reached_nodes(sgNodeID_t n, int radius) {
+    std::set<sgNodeID_t> reached,last={n};
+    for (auto i=0;i<radius;++i) {
+        std::set<sgNodeID_t> new_last;
+        for (auto l:last){
+            for (auto fwl:get_fw_links(l)){
+                new_last.insert(fwl.dest);
+                reached.insert(fwl.dest);
+            }
+        }
+        std::swap(last,new_last);
+    }
+    return reached;
+}
+
+void LinkageDiGraph::remove_transitive_links(int radius) {
+    std::cout<<"removing transitive connections..."<<std::endl;
+
+    //TODO: check mutually transitive connections (A->B, B->C, A->C, C->B)
+    for (auto fn=1;fn<links.size();++fn) {
+        for (auto n:{fn,-fn}) {
+            //create a list of fw nodes reached through each fw connection in up to radius steps
+
+            auto fwl = get_fw_links(n);
+            std::vector<sgNodeID_t> neighbours;
+            std::vector<std::set<sgNodeID_t>> reaches;
+            for (auto fl:fwl){
+                neighbours.push_back(fl.dest);
+                reaches.push_back(fw_reached_nodes(fl.dest,radius));
+            }
+            if (neighbours.empty()) continue;
+            std::set<sgNodeID_t> indirect;
+            for (auto bi=0;bi<neighbours.size();++bi){
+                auto b=neighbours[bi];
+                for (auto ci=0;ci<neighbours.size();++ci){
+                    if (bi==ci) continue;
+                    auto c=neighbours[ci];
+                    //if C is reached through B but B is not through C, then remove connection to C.
+                    if (reaches[bi].count(c)>0 and reaches[ci].count(b)==0){
+                        //std::cout << "Transitive connection detected! " << n << " -> " << b << " -> "
+                        //          << c << std::endl;
+                        //std::cout << "  seq" << llabs(n) << ", seq" << b << ", seq" << llabs(c) << std::endl;
+                        indirect.insert(c);
+                    }
+                }
+
+            }
+            std::cout<<"Connections for node " <<labs(n) << (n>0 ? " FW":" BW")<<":"<<std::endl;
+            for (auto l:neighbours) {
+                std::cout<<l;
+                if (indirect.count(l)>0) std::cout<<" indirect"<<std::endl;
+                else std::cout<<" DIRECT!"<<std::endl;
+            }
+        }
+
+    }
+    std::cout<<"removing transitive connections DONE"<<std::endl;
+}
