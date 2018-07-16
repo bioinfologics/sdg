33b33a59b5233763a697dad0fab4d2abbae91130
Bernardo J. Clavijo <bjclavijo@gmail.com>
2018-07-16 12:12:14 +0100

    PE linkage module, heavily copied from LMP

--- src/sglib/processors/LinkageUntangler.hpp
+++ src/sglib/processors/LinkageUntangler.hpp
@@ -26,6 +26,7 @@ public:
     //Linkage creation methods (work on selected nodes)
     LinkageDiGraph make_topology_linkage(int radius);
     LinkageDiGraph make_paired_linkage(int min_reads);
+    LinkageDiGraph make_paired_linkage_pe(int min_reads);
     LinkageDiGraph make_tag_linkage(int min_tags,float end_perc=.3);
 
     //Linkage filtering methods
