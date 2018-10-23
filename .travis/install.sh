2f4def05474753ee65edc8e8398419e6ed7e2cba
Luis Yanes <yanes.luis@gmail.com>
2018-10-24 00:19:01 +0100

    try to detect new doxygen

--- .travis/install.sh
+++ .travis/install.sh
@@ -14,7 +14,5 @@ if [ "${TRAVIS_OS_NAME}" == linux ]; then
     export DOXYGEN_VER=doxygen-1.8.14
     export DOXYGEN_TAR=${DOXYGEN_VER}.linux.bin.tar.gz
     export DOXYGEN_URL="http://ftp.stack.nl/pub/users/dimitri/${DOXYGEN_TAR}"
-    wget -O - "${DOXYGEN_URL}" | tar xz -C /tmp ${DOXYGEN_VER}/bin/doxygen
-    export PATH="/tmp/${DOXYGEN_VER}/bin:$PATH";
-
+    wget -O - "${DOXYGEN_URL}" | tar xz -C ${HOME} ${DOXYGEN_VER}/bin/doxygen
 fi
