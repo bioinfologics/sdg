6a5c743bb22056827aa8d9327b3d5823b2b5316e
Luis Yanes <yanes.luis@gmail.com>
2018-10-25 19:35:15 +0100

    doxygen-1.8.14 doesn't run on travis, try 1.8.13 instead

--- .travis/install.sh
+++ .travis/install.sh
@@ -11,7 +11,7 @@ if [ "${TRAVIS_OS_NAME}" == linux ]; then
     make install >/dev/null;
 
 
-    export DOXYGEN_VER=doxygen-1.8.14
+    export DOXYGEN_VER=doxygen-1.8.13
     export DOXYGEN_TAR=${DOXYGEN_VER}.linux.bin.tar.gz
     export DOXYGEN_URL="http://ftp.stack.nl/pub/users/dimitri/${DOXYGEN_TAR}"
     wget -O - "${DOXYGEN_URL}" | tar xz -C ${HOME} ${DOXYGEN_VER}/bin/doxygen &&
