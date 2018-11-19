03ec8e399b40ec346da03bebecacded84680518e
Ben J. Ward <ward9250@gmail.com>
2018-11-19 16:25:52 +0000

    Add more types, completely wrap GenericTypes and KmerTypes

--- interfaces/JuliaBSG.cpp
+++ interfaces/JuliaBSG.cpp
@@ -14,10 +14,6 @@ namespace jlcxx {
     template<> struct IsBits<sgNodeStatus_t> : std::true_type {};
 }
 
-typedef std::vector<sgNodeID_t> NodeIDVec;
-typedef std::vector<Link> LinkVec;
-typedef std::vector<Node> NodeVec;
-
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
     // Basic Types //
@@ -32,18 +28,66 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 */
     // Kmer types
     mod.add_type<KmerIDX>("KmerIDX")
-            .constructor();
+            .constructor()
+            .constructor<uint64_t>()
+            .constructor<uint64_t, int32_t, uint32_t, uint8_t>()
+            .method("isless", &KmerIDX::operator<)
+            .method("isgreater", &KmerIDX::operator>)
+            .method("isequal", &KmerIDX::operator==)
+            .method("merge", &KmerIDX::merge)
+            .method("kmer", [](KmerIDX& kidx) -> const uint64_t { return kidx.kmer; })
+            .method("contigID", [](KmerIDX& kidx) -> const int32_t { return kidx.contigID; })
+            .method("pos", [](KmerIDX& kidx) -> const uint32_t { return kidx.pos; })
+            .method("count", [](KmerIDX& kidx) -> const uint8_t { return kidx.count; })
+            .method("<<", [](KmerIDX& kidx){ std::cout << kidx; });
 
     mod.add_type<KmerIDX128>("KmerIDX128")
-            .constructor();
+            .constructor()
+            //.constructor<__uint128_t>()
+            //.constructor<__uint128_t, int32_t, uint32_t, uint8_t>()
+            .method("isless", &KmerIDX128::operator<)
+            .method("isgreater", &KmerIDX128::operator>)
+            .method("isequal", &KmerIDX128::operator==)
+            .method("merge", &KmerIDX128::merge)
+            .method("kmer", [](KmerIDX128& kidx) -> const __uint128_t { return kidx.kmer; })
+            .method("contigID", [](KmerIDX128& kidx) -> const int32_t { return kidx.contigID; })
+            .method("pos", [](KmerIDX128& kidx) -> const uint32_t { return kidx.pos; })
+            .method("count", [](KmerIDX128& kidx) -> const uint8_t { return kidx.count; });
 
     mod.add_type<graphStrandPos>("graphStrandPos")
-            .constructor();
+            .constructor()
+            .constructor<sgNodeID_t, uint32_t>()
+            .method("isequal", &graphStrandPos::operator==)
+            .method("node", [](graphStrandPos& gsp){ return gsp.node; })
+            .method("pos", [](graphStrandPos& gsp){ return gsp.pos; });
+
+    mod.add_type<MinPosIDX>("MinPosIDX")
+            .constructor()
+            .constructor<uint64_t, int32_t>()
+            .method("isless", &MinPosIDX::operator<)
+            .method("isequal", &MinPosIDX::operator==)
+            .method("hash", [](MinPosIDX& mp){ return mp.hash; })
+            .method("pos", [](MinPosIDX& mp){ return mp.pos; })
+            .method("<<", [](MinPosIDX& mp) { std::cout << mp; });
+
+    mod.add_type<kmerPos>("kmerPos")
+            .constructor()
+            .constructor<uint64_t, uint32_t, int32_t>()
+            .method("isless", &kmerPos::operator<)
+            .method("kmer", [](kmerPos& kp) -> const uint64_t { return kp.kmer; })
+            .method("contigID", [](kmerPos& kp) -> const int32_t { return kp.contigID; })
+            .method("offset", [](kmerPos& kp) -> const int32_t { return kp.offset; });
 
     mod.add_type<nodeVisitor>("nodeVisitor")
             .constructor()
             .constructor<sgNodeID_t, unsigned int, unsigned int>()
-            .method("reverseDirection", &nodeVisitor::reverseDirection);
+            .method("reverseDirection", &nodeVisitor::reverseDirection)
+            .method("<<", [](nodeVisitor& nv){ std::cout << nv; })
+            .method("isless", &nodeVisitor::operator<)
+            .method("isequal", &nodeVisitor::operator==)
+            .method("node", [](nodeVisitor& nv) -> const sgNodeID_t { return nv.node; })
+            .method("dist", [](nodeVisitor& nv) -> const unsigned int { return nv.dist; })
+            .method("path_length", [](nodeVisitor& nv) -> const unsigned int { return nv.path_length; });
 
     mod.add_bits<sgNodeStatus_t>("sgNodeStatus_t");
     mod.set_const("sgNodeActive", sgNodeActive);
@@ -56,7 +100,8 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
             .method("make_rc", &Node::make_rc)
             .method("sequence", [](Node& nd){ return nd.sequence; })
             .method("status", [](Node& nd){ return nd.status; })
-            .method("==", &Node::operator==);
+            .method("==", &Node::operator==)
+            .method("<<", [](Node& n){ std::cout << n; });
 
     mod.add_type<Link>("Link")
             .constructor()
@@ -64,9 +109,23 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
             .method("source", [](Link& lnk){ return lnk.source; })
             .method("dest", [](Link& lnk){ return lnk.dest; })
             .method("dist", [](Link& lnk){ return lnk.dist; })
-            .method("==", &Link::operator==)
-            .method("lessthan", &Link::operator<);
+            .method("isequal", &Link::operator==)
+            .method("isless", &Link::operator<)
+            .method("<<", [](Link& l){ std::cout << l; });
 
+    mod.add_type<jlcxx::Parametric<jlcxx::TypeVar<1>>>("BSGVector")
+        .apply<std::vector<Node>,
+                std::vector<sgNodeID_t>,
+                std::vector<Link>,
+                std::vector<nodeVisitor>,
+                std::vector<SequenceGraphPath>,
+                std::vector<SequenceSubGraph>,
+                std::vector<std::vector<sgNodeID_t>>>([](auto wrapped){
+            typedef typename decltype(wrapped)::type WrappedT;
+            wrapped.method("size", &WrappedT::size);
+            wrapped.method("at", [](WrappedT& vec, size_t i){ return vec.at(i); });
+    });
+/*
     mod.add_type<NodeIDVec>("NodeIDVec")
             .method("size", &NodeIDVec::size)
             .method("at", [](NodeIDVec& vec, size_t i){
@@ -82,6 +141,17 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
             .method("at", [](NodeVec& vec, size_t i){
                 return vec.at(i);
             });
+            */
+
+    mod.add_type<jlcxx::Parametric<jlcxx::TypeVar<1>>>("BSGSet")
+        .apply<std::set<nodeVisitor>>([](auto wrapped){
+            typedef typename decltype(wrapped)::type WrappedT;
+            wrapped.constructor();
+            wrapped.method("size", &WrappedT::size);
+            wrapped.method("clear", &WrappedT::clear);
+            //wrapped.method("insert", &WrappedT::insert);
+            //wrapped.method("erase", &WrappedT::erase);
+        });
 
     // Graph types
     mod.add_type<SequenceGraph>("SequenceGraph")
@@ -102,18 +172,27 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
             .method("is_sane", &SequenceGraph::is_sane)
             .method("get_loopy_nodes", &SequenceGraph::get_loopy_nodes)
             .method("get_flanking_nodes", &SequenceGraph::get_flanking_nodes)
-            //.method("depth_first_search", &SequenceGraph::depth_first_search)
-            //.method("breath_first_search", &SequenceGraph::breath_first_search)
-            //.method("find_path_between", &SequenceGraph::find_path_between)
-            //.method("explore_nodes", &SequenceGraph::explore_nodes)
+            .method("depth_first_search", &SequenceGraph::depth_first_search)
+            .method("breath_first_search", &SequenceGraph::breath_first_search)
+            .method("find_path_between", &SequenceGraph::find_path_between)
+            .method("explore_nodes", &SequenceGraph::explore_nodes)
             .method("remove_node", &SequenceGraph::remove_node)
             .method("remove_link", &SequenceGraph::remove_link)
+            .method("join_path", &SequenceGraph::join_path)
+            .method("join_all_unitigs", &SequenceGraph::join_all_unitigs)
+            //.method("get_all_unitigs", &SequenceGraph::get_all_unitigs)
+            //.method("expand_node", &SequenceGraph::expand_node)
+                    //.method("get_distances_to", &SequenceGraph::get_distances_to)
+            //.method("get_all_bubbly_subgraphs", &SequenceGraph::get_all_bubbly_subgraphs)
+            //.method("print_bubbly_subgraph_stats", &SequenceGraph::print_bubbly_subgraph_stats)
             .method("oldnames_to_nodes", &SequenceGraph::oldnames_to_nodes)
             .method("get_node", &SequenceGraph::get_node)
+                    //.method("is_loop", &SequenceGraph::is_loop)
             .method("link_exists", &SequenceGraph::link_exists)
-            .method("nodeID_to_name", &SequenceGraph::nodeID_to_name);
+            .method("nodeID_to_name", &SequenceGraph::nodeID_to_name)
+            .method("count_active_nodes", &SequenceGraph::count_active_nodes);
+            //.method("find_all_paths_between", &SequenceGraph::find_all_paths_between);
 
-/*
     mod.add_type<SequenceGraphPath>("SequenceGraphPath")
             .constructor<SequenceGraph&, const std::vector<sgNodeID_t>>()
             .constructor<const SequenceGraph&, const std::vector<sgNodeID_t>>()
@@ -124,13 +203,13 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
             .method("get_next_links", &SequenceGraphPath::get_next_links)
             .method("reverse", &SequenceGraphPath::reverse)
             .method("is_canonical", &SequenceGraphPath::is_canonical)
-            .method("make_set_of_nodes", &SequenceGraphPath::make_set_of_nodes)
+            //.method("make_set_of_nodes", &SequenceGraphPath::make_set_of_nodes)
             .method("append_to_path", &SequenceGraphPath::append_to_path)
             .method("extend_if_coherent", &SequenceGraphPath::extend_if_coherent)
             .method("clear", &SequenceGraphPath::clear)
             .method("is_unitig", &SequenceGraphPath::is_unitig)
             .method("getNodes", static_cast<std::vector<sgNodeID_t>& (SequenceGraphPath::*)()>(&SequenceGraphPath::getNodes));
-*/
+
  /*
     mod.add_type<SequenceSubGraph>("SequenceSubGraph")
             .constructor<const SequenceGraph&, std::vector<sgNodeID_t>>()
@@ -257,6 +336,8 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
     // Workspaces
     mod.add_type<WorkSpace>("WorkSpace")
             .constructor()
+            .method("print_log", &WorkSpace::print_log)
+            .method("add_log_entry", &WorkSpace::add_log_entry)
             .method("load_from_disk", &WorkSpace::load_from_disk)
             .method("dump_to_disk", &WorkSpace::dump_to_disk)
             //.method("getKCI", &WorkSpace::getKCI)
