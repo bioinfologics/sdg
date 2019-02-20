f57dff59cece30302eb6f2510d16cdb57ae7777b
Bernardo J. Clavijo <bjclavijo@gmail.com>
2019-02-20 15:39:03 +0000

    remove obsolete unused SG::find_path_between

--- src/sglib/graph/SequenceGraph.cc
+++ src/sglib/graph/SequenceGraph.cc
@@ -751,54 +751,6 @@ SequenceGraph::depth_first_search(const nodeVisitor seed, unsigned int size_limi
     return result;
 }
 
-SequenceGraphPath
-SequenceGraph::find_path_between(const sgNodeID_t seed, const sgNodeID_t target, unsigned int size_limit,
-                                 unsigned int edge_limit) {
-    struct visitor {
-        sgNodeID_t node;
-        uint dist;
-        uint path_length;
-        visitor(sgNodeID_t n, uint d, uint p) : node(n), dist(d), path_length(p) {}
-    };
-
-    std::vector<SequenceGraphPath> collected_paths;
-    std::vector<sgNodeID_t> current_path;
-
-    std::stack<visitor> to_visit;
-    to_visit.emplace(seed, 0, 0);
-    std::set<sgNodeID_t> visited;
-
-    while (!to_visit.empty()) {
-        const auto activeNode(to_visit.top());
-        to_visit.pop();
-        if (visited.find(activeNode.node) == visited.end() and (activeNode.path_length < edge_limit or edge_limit==0) and (activeNode.dist < size_limit or size_limit==0) )
-        {
-            visited.emplace(activeNode.node);
-
-            if (current_path.size() > activeNode.path_length) {
-                current_path[activeNode.path_length] = activeNode.node;
-                current_path.resize(activeNode.path_length + 1);
-            } else {
-                current_path.emplace_back(activeNode.node);
-            }
-
-            for (const auto &l : get_fw_links(activeNode.node)) {
-                // For each child of the current node, create a child visitor object, and enter it into the parent map.
-                visitor child { l.dest, activeNode.dist + uint(nodes[std::abs(l.dest)].sequence.length()), activeNode.path_length + 1 };
-
-                // If I've found the target along this dfs path, I need to collect it and put it into a SequenceGraphPath object.
-                if (child.node == target) {
-                    current_path.emplace_back(target);
-                    return SequenceGraphPath(*this,current_path);
-                } else {
-                    to_visit.emplace(child);
-                }
-            }
-        }
-    }
-    return SequenceGraphPath{*this};
-}
-
 std::vector<nodeVisitor>
 SequenceGraph::explore_nodes(std::vector<std::string> &nodes, unsigned int size_limit, unsigned int edge_limit) {
     std::set<nodeVisitor> resultNodes;
