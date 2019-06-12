24b35dd38ec213abddc0fdadc1e62aa167818635
Bernardo J. Clavijo <bjclavijo@gmail.com>
2019-06-12 09:47:03 +0100

    alternative two-pass index construction

--- src/sglib/indexers/SatKmerIndex.hpp
+++ src/sglib/indexers/SatKmerIndex.hpp
@@ -81,6 +81,66 @@ public:
         if (verbose) sglib::OutputLog() << "DONE" << std::endl;
     }
 
+    /**
+     * This function is a alternative generator of the kmer index, reserving space first in the internal vectors.
+     *
+     * @param sg
+     * @param filter_limit
+     * @param verbose
+     */
+    void generate_index_alt(const SequenceGraph &sg, uint8_t filter_limit = 200, bool verbose=true) {
+        // this can be parallelised by contig by aggregating different k_usage vectors on the first step, second and third steps are trickier
+        // This two-pass approach could be easily adapted to a single memory block rather than vector of vectors
+        uint64_t indexed_positions(0),indexed_kmers(0),total_kmers(0);
+        //---- First Step, count k-mer occupancy ----//
+        if (verbose) sglib::OutputLog() << "First pass: analysing usage over " << std::to_string(uint64_t (std::pow(4,k))) << " k-mers..."<<std::endl;
+        std::vector<uint8_t> k_usage(std::pow(4,k));
+        std::vector<std::pair<bool,uint64_t > > contig_kmers;
+        contig_kmers.reserve(1000000); //1Mbp per contig to start with?
+        StringKMerFactory skf(k);
+        for (sgNodeID_t n = 1; n < sg.nodes.size(); ++n) {
+            if (sg.nodes[n].sequence.size() >= k) {
+                contig_kmers.clear();
+                skf.create_kmers(sg.nodes[n].sequence, contig_kmers);
+                for (const auto &kmer:contig_kmers) if (kmer.second<=filter_limit) ++k_usage[kmer.second]; //beware of overflow, hence stop counting after reaching limit
+            }
+        }
+        //---- Second Step, reserve space for each vector in structure (avoiding reallocations and such)----//
+        if (verbose) sglib::OutputLog() << "Reserving space on internal vectors..." << std::endl;
+        assembly_kmers.clear();
+        assembly_kmers.reserve(k_usage.size());
+        for (auto &count:k_usage){
+            assembly_kmers.emplace_back();
+            if (count<=filter_limit) {
+                assembly_kmers.back().reserve(count); //Already accounts for the filter
+                ++indexed_kmers;
+            }
+        }
+        if (verbose) sglib::OutputLog() << "Space reserved to index "<< indexed_kmers <<" k-mers with less than "<<filter_limit+1<<" copies..." << std::endl;
+
+        //---- Third step, actually save the positions ---//
+        if (verbose) sglib::OutputLog() << "Second pass: filling index..." << std::endl;
+        contig_kmers.reserve(10000);
+        for (sgNodeID_t n = 1; n < sg.nodes.size(); ++n) {
+            if (sg.nodes[n].sequence.size() >= k) {
+                contig_kmers.clear();
+                skf.create_kmers(sg.nodes[n].sequence, contig_kmers);
+                int k_i(0);
+                for (const auto &kmer:contig_kmers) {
+                    if (k_usage[kmer.second]<=filter_limit){ //filter, only inserts on relevant k-mers, which are already reserved too
+                        assembly_kmers[kmer.second].emplace_back(n, kmer.first ? k_i + 1 : -(k_i + 1)); //is this really how we save the offsets on the indexes?
+                        ++indexed_positions;
+                    }
+                    ++k_i;
+                }
+                total_kmers+=contig_kmers.size();
+            }
+        }
+
+        if (verbose) sglib::OutputLog() << indexed_positions << "/" << total_kmers << " postions indexed by " << indexed_kmers << " kmers" << std::endl;
+
+    }
+
     void generate_index_parallel(const SequenceGraph &sg, int filter_limit = 200, bool verbose=true) {
         uint64_t total_kmers(0);
         assembly_kmers.resize(std::pow(4,k));
