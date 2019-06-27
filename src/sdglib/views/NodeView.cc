6761ae64cdd4788d64a315dd77311b0ad031d645
Bernardo J. Clavijo <bjclavijo@gmail.com>
2019-06-27 10:43:20 +0100

    work on NodeView

--- src/sdglib/views/NodeView.cc
+++ src/sdglib/views/NodeView.cc
@@ -3,3 +3,55 @@
 //
 
 #include "NodeView.hpp"
+#include <sdglib/graph/SequenceDistanceGraph.hpp>
+
+std::string NodeView::sequence() const {
+    return dg.sdg.get_node_sequence(node_id);
+}
+
+uint64_t NodeView::size() const {
+    return dg.sdg.get_node_size(node_id);
+}
+
+std::vector<NodeDistanceView> NodeView::next() const {
+    auto fwl=dg.get_fw_links(node_id);
+    std::vector<NodeDistanceView> r;
+    r.reserve(fwl.size());
+    for (auto &l:fwl) {
+        r.emplace_back(NodeView(dg,l.dest),l.dist,l.support);
+    }
+    return r;
+}
+
+std::vector<NodeDistanceView> NodeView::prev() const {
+    auto bwl=dg.get_bw_links(node_id);
+    std::vector<NodeDistanceView> r;
+    r.reserve(bwl.size());
+    for (auto &l:bwl) {
+        r.emplace_back(NodeView(dg,-l.dest),l.dist,l.support);
+    }
+    return r;
+}
+
+std::ostream &operator<<(std::ostream &os, const NodeView &nv) {
+    os << "< NodeView: "<<nv.node_id<<" in "<<nv.dg.name<<" >";
+    return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const NodeDistanceView &ndv) {
+    os << "< NodeDistanceView: "<<ndv.distance<<"bp to "<<ndv.node.node_id<<" >";
+    return os;
+}
+
+NodeView& NodeView::operator=(NodeView const &o) {
+    if (&o == this) return *this;
+    dg=o.dg;
+    node_id=o.node_id;
+}
+
+NodeDistanceView& NodeDistanceView::operator=(NodeDistanceView const &o) {
+    if (&o == this) return *this;
+    node=o.node;
+    distance=o.distance;
+    support=o.support;
+}
--- src/sdglib/views/NodeView.hpp
+++ src/sdglib/views/NodeView.hpp
@@ -4,34 +4,42 @@
 
 #pragma once
 
-#include <sdglib/graph/DistanceGraph.hpp>
+#include <sdglib/graph/SequenceDistanceGraph.hpp>
 
 class NodeDistanceView;
 
 class NodeView {
-    NodeView(const DistanceGraph &_dg,sgNodeID_t _n):dg(_dg),node_id(_n){};
+public:
+    NodeView(DistanceGraph &_dg,sgNodeID_t _n):dg(_dg),node_id(_n){};
+    NodeView& operator=(NodeView const &o);
+    friend std::ostream &operator<<(std::ostream &os, const NodeView &n);
+    std::string sequence() const;
+    uint64_t size() const;
     std::vector<NodeDistanceView> next() const;
     std::vector<NodeDistanceView> prev() const;
-    std::string sequence() const;
-    std::vector<uint16_t> kmer_coverage(std::string kcovds_name,int kcovds_count_name) const;
-    std::vector<uint16_t> kmer_coverage(int kcovds_idx,int kcovds_count_idx) const;
-    std::vector<seqID_t> preads(int prds_idx=0) const;
-    std::vector<seqID_t> preads(std::string prds_name) const;
-    std::vector<seqID_t> lireads(int lirds_idx=0) const;
-    std::vector<seqID_t> lireads(std::string lirds_name) const;
-    void li_tag_neihgbours(int lirds_idx=0) const;
-    void li_tag_neihgbours(std::string lirds_name) const;
-    void li_tags(int lirds_idx=0) const;
-    void li_tags(std::string lirds_name) const;
-    std::vector<seqID_t> loreads(int lords_idx=0) const;
-    std::vector<seqID_t> loreads(std::string lords_name) const;
-    std::vector<seqID_t> readpaths(int rpds_idx=0) const;
-    std::vector<seqID_t> readpaths(std::string rpds_name) const;
-    const DistanceGraph &dg;
-    const sgNodeID_t node_id;
+//    std::vector<uint16_t> kmer_coverage(std::string kcovds_name,int kcovds_count_name) const;
+//    std::vector<uint16_t> kmer_coverage(int kcovds_idx,int kcovds_count_idx) const;
+//    std::vector<seqID_t> preads(int prds_idx=0) const;
+//    std::vector<seqID_t> preads(std::string prds_name) const;
+//    std::vector<seqID_t> lireads(int lirds_idx=0) const;
+//    std::vector<seqID_t> lireads(std::string lirds_name) const;
+//    void li_tag_neihgbours(int lirds_idx=0) const;
+//    void li_tag_neihgbours(std::string lirds_name) const;
+//    void li_tags(int lirds_idx=0) const;
+//    void li_tags(std::string lirds_name) const;
+//    std::vector<seqID_t> loreads(int lords_idx=0) const;
+//    std::vector<seqID_t> loreads(std::string lords_name) const;
+//    std::vector<seqID_t> readpaths(int rpds_idx=0) const;
+//    std::vector<seqID_t> readpaths(std::string rpds_name) const;
+    DistanceGraph &dg;
+    sgNodeID_t node_id;
 };
 
 class NodeDistanceView {
+public:
+    NodeDistanceView(const NodeView &nv,const int32_t &d, const Support &s):node(nv),distance(d),support(s){};
+    NodeDistanceView& operator=(NodeDistanceView const &o);
+    friend std::ostream &operator<<(std::ostream &os, const NodeDistanceView &ndv);
     NodeView node;
     int32_t distance;
     Support support;
