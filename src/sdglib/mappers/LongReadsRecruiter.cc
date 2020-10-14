1a13ff7c6ac2adf0ce3d44d0e4f2cf7b69be7db1
Gonzalo <gonzalogacc@gmail.com>
2020-10-14 18:14:20 +0100

    Pragma dynamic scheduling

--- src/sdglib/mappers/LongReadsRecruiter.cc
+++ src/sdglib/mappers/LongReadsRecruiter.cc
@@ -554,7 +554,7 @@ void LongReadsRecruiter::map(uint16_t seed_size, uint64_t first_read, uint64_t l
         PerfectMatchExtender pme(sdg,k);
         ReadSequenceBuffer sequence_reader(datastore);
         uint64_t private_mapped_reads=0;
-#pragma omp for
+#pragma omp for schedule(dynamic,10000)
         for (auto rid=first_read;rid<=last_read;++rid){
 
             std::vector<PerfectMatch> private_read_perfect_matches;
