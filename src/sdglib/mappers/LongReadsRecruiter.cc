12cf382b9948b1239f151bf5a7efe8bf6c6bca95
Gonzalo <gonzalogacc@gmail.com>
2021-06-16 20:23:24 +0100

    Some kmer number reports on NKI to check the index density (important to pick K)

--- src/sdglib/mappers/LongReadsRecruiter.cc
+++ src/sdglib/mappers/LongReadsRecruiter.cc
@@ -553,6 +553,7 @@ std::vector<PerfectMatch> LongReadsRecruiter::reverse_perfect_matches(const std:
 
 void LongReadsRecruiter::map(uint16_t seed_size, uint64_t first_read, uint64_t last_read) {
     if (seed_size<k) seed_size=k;
+    sdglib::OutputLog()<<"Creating index!"<<std::endl;
     NKmerIndex nki(sdg,k,f);
     sdglib::OutputLog()<<"Index created!"<<std::endl;
     if (last_read==0) last_read=datastore.size();
