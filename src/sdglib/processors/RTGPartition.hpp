12296bfb7265eab757904277d8cff9519355092a
Bernardo J. Clavijo <bjclavijo@gmail.com>
2021-11-05 12:43:11 +0000

    Cleanup on RTGPArtition

--- src/sdglib/processors/RTGPartition.hpp
+++ src/sdglib/processors/RTGPartition.hpp
@@ -29,34 +29,9 @@ public:
 
     uint64_t propagate(uint64_t steps=UINT64_MAX, bool verbose=false);
 
-    uint64_t thread_propagate(uint64_t steps=UINT64_MAX, float vote_perc=.1, int max_noise=3, bool verbose=false);
-
-    std::map<uint64_t, uint64_t> thread_class_votes(uint64_t tid, std::set<ThreadOverlapType> ovltypes={});
-
-    uint64_t thread_propagate2(uint64_t steps=UINT64_MAX, int min_side_votes=3, float side_vote_perc=.75, int min_contain_votes=5, float contain_vote_perc=.90,  bool reclassify= false, bool verbose=false);
-
     std::unordered_map<std::pair<int64_t,int64_t>,std::vector<int64_t>> find_class_bridges(int p, int q);
-    //find_class_bridges -> threads on class 0, where %threads on nodes switches from start to end between two classes
-        //could also be done will all thread's p/q, then comparing all p/q's
-    //is_class_mixed: -> can use lines to check? also, connectivity/subclasses
-
-    void compute_thread_intersections(int min_threads, int max_threads);
-
-    void compute_thread_neighbours(int min_shared=10);
-
-    void compute_thread_neighbours_p_q(int p=10, int q=10);
-
-    std::vector<int64_t> get_thread_neighbours(int64_t tid) const;
-
-    std::vector<ThreadOverlapType> get_thread_neighbours_types(int64_t tid) const;
 
     void reset(int min_node_threads=-1,float node_min_percentage=-1, int thread_p=-1, int thread_q=-1);
-
-    int64_t get_thread_intersection(int64_t tid1, int64_t tid2) const;
-
-    std::vector<int> thread_shared_detail(int64_t tid1, int64_t tid2) const;
-
-    void classify_neighbours(int skip_nodes=10, bool discard_invalid=true);
 
     const ReadThreadsGraph &rtg;
     int min_node_threads;
@@ -70,9 +45,6 @@ public:
     std::unordered_map<int64_t ,int64_t> thread_class;
     std::unordered_set<sgNodeID_t> nodes_to_evaluate;
     std::unordered_set<int64_t> threads_to_evaluate;
-    std::unordered_map<std::pair<int64_t, int64_t> ,int64_t> thread_intersections;
-    std::unordered_map<int64_t,std::vector<int64_t>> thread_neighbours;
-    std::unordered_map<int64_t,std::vector<ThreadOverlapType>> thread_neighbours_types;
 
 private:
     bool thread_available(uint64_t tid, int min_nodes=50, float max_classified_nodes_perc=.05);
